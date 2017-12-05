4485e331b451a58849206ced916c20620762810c
Maciej Duda <maciej.duda1@gazeta.pl>
2017-12-05 23:41:05 +0100

    v. 1.0 podstawowa funkcjonalność

--- modules/server.js
+++ modules/server.js
@@ -1,12 +1,28 @@
-﻿var http = require('http');
+var http = require('http');
 var colors = require('colors');
+
+var handlers = require('./handlers.js');
 
 function start(){
 	function onRequest(request, response){
-		console.log("Odebrano zapytanie!");
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Pierwsze Koty za płoty!");
-		response.end();
+		console.log("Odebrano zapytanie!".green);
+		console.log("zapytanie " + request.url + " Odebrane");
+		response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+
+		switch (request.url){
+			case '/':
+			case '/start':
+				handlers.welcome(request, response);
+				break;
+			case '/upload': 
+				handlers.upload(request, response);
+				break;
+			case '/show':
+				handlers.show(request, response);
+				break;
+			default:
+				handlers.error(request, response);
+		}
 	}
 	http.createServer(onRequest).listen(9000);
 	console.log('Uruchomiono server!'.green);
